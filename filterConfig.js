7774e6cc9a7ec6c8e44d544cb9b2005117100351
Vince Pendergrass <vpendergrass@keyholesoftware.com>
2017-02-02 13:07:43 -0600

    update of filterConfig.js

--- filterConfig.js
+++ filterConfig.js
@@ -4,7 +4,7 @@ module.exports = {
 		apiPattern: "/sherpa/.*",	
 		serviceName: "Grokola",
 		grokolaServer: "https://beta.grokola.com",
-		token: "3a4f66ac-4793-4c1f-9c6d-4c5ee1afd0f1",
+		token: "9a019713-f958-4b70-b79b-74090a0ea9f7",
 		referenceId: "279",
 		watchThreshold: "1",
 		sleep: "5000"
